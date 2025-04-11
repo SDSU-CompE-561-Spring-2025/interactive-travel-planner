29931bc2b83485453b8cf981c4cf7389b76e2b10
RojinOsman <rojinosman1988@gmail.com>
2025-04-11 00:00:11 -0700

    "added a CRUD opperation to create a user trip"

---
+++ backend/src/app/services/trips.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+from app.models.trip import Trip
+from app.schemas.trips import TripCreate
+
+def create_user_trip(db: Session, trip: TripCreate, user_id: int):
+    db_trip = Trip(**trip.dict(), user_id=user_id)
+    db.add(db_trip)
+    db.commit()
+    db.refresh(db_trip)
+    return db_trip
