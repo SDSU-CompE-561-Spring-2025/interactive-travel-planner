b06b447c99d58bf2af3e8a800556d7d9dd4a1843
Primordiou <primordiou@gmail.com>
2025-04-12 14:41:58 -0700

    updated destinations route and schema

--- backend/src/app/routes/destinations.py
+++ backend/src/app/routes/destinations.py
@@ -1,29 +1,83 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
-from app.schemas.destinations import Destinations
+from app.schemas.destinations import DestinationsCreate, DestinationResponse
+from app.models.destinations import Destinations
 
 router = APIRouter()
 
-@router.get("/trips/{trip_id}/destinations")
+@router.get("/trips/{trip_id}/destinations", response_model = DestinationResponse)
 def get_trip_destinations(trip_id: int, db: Session = Depends(get_db)):
-    return {"message": f"Destinations for trip {trip_id} returned successfully"}
+    destinations = db.query(Destinations).filter(Destinations.trip_id == trip_id).all()
+    
+    if not destinations:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Destination for {trip_id} not found")
 
+    return destinations
+    return {"message": f"Destination {destination_id} returned successfully"}
 
-@router.post("/trips/{trip_id}/destinations")
+@router.post("/trips/{trip_id}/destinations", response_model = DestinationResponse)
 def create_trip_destination(trip_id: int, db: Session = Depends(get_db)):
-    return {"message": f"Destination created for trip {trip_id}"}
+    destinations = Destinations(**destination.dict(), trip_id = trip_id)
+    
+    trip = db.query(TripModel).get(trip_id)
+    if not trip:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Trip with ID {trip_id} not found"
+        )
+
+    db.add(destinations)
+    db.commit()
+    db.refresh(destinations)
+    return DestinationResponse
+    #return {"message": f"Destination created for trip {trip_id}"}
 
 @router.get("/destinations/{destination_id}")
 def get_destination(destination_id: int, db: Session = Depends(get_db)):
-    return {"message": f"Destination {destination_id} returned successfully"}
+    destinations = db.query(Destinations).get(destination_id)
+
+    if not destinations:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Destination for {trip_id} not found")
+    
+    return destinations
+    #return {"message": f"Destination {destination_id} returned successfully"}
 
 
 @router.put("/destinations/{destination_id}")
 def update_destination(destination_id: int, db: Session = Depends(get_db)):
-    return {"message": f"Destination {destination_id} updated successfully"}
+    destinations = db.query(Destinations).get(destination_id)
+
+    if not destinations:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Destination for {trip_id} not found")
+    
+    for key, value in update_destination.dict().items():
+        setattr(destination, key, value)
+    
+    db.commit()
+    db.refresh(destinations)
+
+    return destinations
+    #return {"message": f"Destination {destination_id} updated successfully"}
 
 
 @router.delete("/destinations/{destination_id}")
 def delete_destination(destination_id: int, db: Session = Depends(get_db)):
-    return {"message": f"Destination {destination_id} deleted successfully"}
+    destinations = db.query(Destinations).get(destination_id)
+
+    if not destinations:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = f"Destination for {trip_id} not found")
+
+    db.delete(destinations)
+    db.commit()
+    
+    return None 
+    #return {"message": f"Destination {destination_id} deleted successfully"}
