e90e2efcfe32c7ded34868565752cc173afffcbb
Ethan <equach7914@sdsu.edu>
2025-04-12 22:06:51 -0700

    pulled changes to main

---
+++ backend/src/app/models/destinations.py
@@ -0,0 +1,23 @@
+from datetime import UTC, datetime
+import enum
+
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.core.database import Base
+
+
+
+class Destinations(Base)
+    __tablename__ = "destinations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    trip_id = Column(Integer, ForeignKey("trips.id"))
+    name = Column(String)
+    description = Column(String)
+    location = Column(String)
+    order = Column(Integer)
+    created_at = Column(DateTime, default=datetime.now(UTC))
+    updated_at = Column(DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC))
+
+    trip = relationship("Trips", back_populates="destinations")
