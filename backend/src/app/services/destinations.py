559d0bec10fe31eca8f42d0a8e0306d502b60730
RojinOsman <rojinosman1988@gmail.com>
2025-04-12 21:59:58 -0700

    added the create CRUD opperation in services/desinations.py

--- backend/src/app/services/destinations.py
+++ backend/src/app/services/destinations.py
@@ -4,3 +4,11 @@ from app.schemas.destinations import DestinationCreate
 
 def get_destinations_by_trip(trip_id: int, db: Session):
     return db.query(Destination).filter(Destination.trip_id == trip_id).all()
+
+
+def create_destination(trip_id: int, destination: DestinationCreate, db: Session):
+    db_destination = Destination(**destination.dict(), trip_id=trip_id)
+    db.add(db_destination)
+    db.commit()
+    db.refresh(db_destination)
+    return db_destination
