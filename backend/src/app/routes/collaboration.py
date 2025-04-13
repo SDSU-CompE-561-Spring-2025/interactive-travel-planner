75e12745ffe662500a903689c9424156438329a0
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 01:27:07 -0700

    add post route and get route to create and retrieve collaborations

--- backend/src/app/routes/collaboration.py
+++ backend/src/app/routes/collaboration.py
@@ -6,3 +6,10 @@ from app.database import get_db
 
 router = APIRouter()
 
+@router.post("/collaborations/", response_model=Collaboration)
+def add_collaborator(collab: CollaborationCreate, db: Session = Depends(get_db)):
+    return crud_collaboration.create_collaboration(db=db, collab=collab)
+
+@router.get("/collaborations/{trip_id}", response_model=list[Collaboration])
+def get_collaborators(trip_id: int, db: Session = Depends(get_db)):
+    return crud_collaboration.get_collaborations_by_trip(db=db, trip_id=trip_id)
