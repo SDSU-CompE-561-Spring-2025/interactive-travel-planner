0dd87ca6de7520fbc639cd1359e7307ee052ecb3
RojinOsman <rojinosman1988@gmail.com>
2025-04-12 21:27:51 -0700

    pulled the most recent changes from git

--- backend/src/app/routes/__init__.py
+++ backend/src/app/routes/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.routes import category, user, trips, destinations
+from app.routes import category, user, trips, destinations, itinerary
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/auth", tags=["User"])
 api_router.include_router(category.router, prefix="/category", tags=["Category"])
 api_router.include_router(trips.router, tags=["Trips"])
 api_router.include_router(destinations.router, tags=["Destinations"])
+api_router.include_router(itineray.router, tags=["Itinerary"])
