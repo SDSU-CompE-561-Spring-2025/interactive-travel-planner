8b35f41249cfd8136fdb9521a5f43a4ae780c20c
Ethan <equach7914@sdsu.edu>
2025-04-12 22:21:44 -0700

    define model class collaboration

---
+++ backend/src/app/models/collaboration.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class Collaboration(Base):
