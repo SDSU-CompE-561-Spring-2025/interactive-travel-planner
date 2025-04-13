71afa43884ca5ee5425a016c3a4df92d0d5196ad
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 16:40:36 -0700

    fixed init file

--- backend/src/app/__init__.py
+++ backend/src/app/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.routers.calendar import calendar
+from app.routes.calendar import calendar
 
 api_router = APIRouter()
 api_router.include_router(calendar.router, prefix="/api", tags=["calendar"])
