921865c067309d0131e19896bf08671dff24fd82
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 13:28:39 -0700

    add dependencies for updating trip dates

--- backend/src/app/services/calendar.py
+++ backend/src/app/services/calendar.py
@@ -1,15 +1,3 @@
-from sqlalchemy import Column, Integer Date
-from app.database import Base
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationshp
-
-class Trip(Baes):
-    __tablename__ = "trips"
-
-    id = Column(Integer, primary_key=True, index=True)
-    start_date = Column(Date, nullable=True)
-    end_date = Column(Date, nullable=True)
-
-    user_id = Column(Integer, ForeignKey("users.id"))
-    user = relationship("User", back_populates="trips")
-
+from sqlalchemy.orm import Session
+from app.models.trip import Trip
+from app.schemas.calendar import CalendarCreate
