482d7878083362968750155038061f072d00f267
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 01:19:33 -0700

    initialize API router for collaboration routes

--- backend/src/app/routes/collaboration.py
+++ backend/src/app/routes/collaboration.py
@@ -3,3 +3,6 @@ from sqlalchemy.orm import Session
 from app.schemas import CollaborationCreate, Collaboration
 from app.crud import collaboration as crud_collaboration
 from app.database import get_db
+
+router = APIRouter()
+
