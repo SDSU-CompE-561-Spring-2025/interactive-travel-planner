b3645fa64df0cf9f9ed4770edd5ddb23d0133322
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 14:55:47 -0700

    initialize api router instance

--- backend/src/app/routes/calendar.py
+++ backend/src/app/routes/calendar.py
@@ -3,3 +3,5 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.calendar import CalenderCreate, CalendarResponse
 from app.crud import calender as crud_calendar
+
+router = APIRouter()
