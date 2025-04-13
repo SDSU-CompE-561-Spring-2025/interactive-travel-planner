7de0e2eda468f483c1d917773966b52023dd4bf6
Ethan Quach <ethanquachwork@gmail.com>
2025-04-12 23:04:30 -0700

    define columns and store data: user, trip, role, date added

--- backend/src/app/models/collaboration.py
+++ backend/src/app/models/collaboration.py
@@ -5,3 +5,9 @@ from app.database import Base
 
 class Collaboration(Base):
     __tablename__ = 'collaborations'
+
+    id = Column(Integer, primary_key=True, index=True)
+    trip_id = Column(Integer, ForeignKey('trips.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    role = Column(String, nullable=False, default='viewer')
+    invited_at = Column(DateTime, default=datetime.utcnow)
