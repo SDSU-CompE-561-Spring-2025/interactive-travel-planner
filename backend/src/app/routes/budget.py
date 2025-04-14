bbea72d90a8bd576eade1ded02cfd490f1d47a7f
kohkikita <130106015+kohkikita@users.noreply.github.com>
2025-04-13 21:16:53 -0700

    updated get request

--- backend/src/app/routes/budget.py
+++ backend/src/app/routes/budget.py
@@ -7,8 +7,18 @@ from app.models.budget import Budget
 
 router = APIRouter()
 
+# Retrieve budget based off of trip_id
 @router.get("/trips/{tripId}/budget", )
 async def get_trip_budget():
+    budget = db.query(Budget).filter(Budget.trip_id == trip_id).all()
+    
+    if not Budget:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No budget found for trip {trip_id}"
+        )
+
+    return Budget
 
 @router.post("/trips/{tripId}/budget", )
 async def create_trip_budget():
