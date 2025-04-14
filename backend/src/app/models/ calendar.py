e4a4c1193565599077af1499ab747a0dfdc6a587
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 18:35:48 -0700

    fixed small naming error

--- backend/src/app/models/ calendar.py
+++ backend/src/app/models/ calendar.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from database import Base
+from app.core.database import Base
 
 class CalendarEvent(Base):
     __tablename__ = "calendar_events"
