f40acc989f168e230b0f69b7a7e48356f1c5146c
kohkikita <130106015+kohkikita@users.noreply.github.com>
2025-04-13 21:11:52 -0700

    updated layout for budget routes

--- backend/src/app/routes/budget.py
+++ backend/src/app/routes/budget.py
@@ -1,8 +1,20 @@
 fromfrom fastapi import APIRouter
-from app.schemas.budget import 
 from app.dependencies import get_db
 from fastapi import Depends
 from sqlalchemy.orm import Session
+from app.schemas.budget import 
+from app.models.budget import Budget
 
-route = APIRouter()
+router = APIRouter()
 
+@router.get("/trips/{tripId}/budget", )
+async def get_trip_budget():
+
+@router.post("/trips/{tripId}/budget", )
+async def create_trip_budget():
+
+@router.put("/budget/{budgetId}", )
+async def update_budget():
+
+@router.delete("budget/{budgetId}", )
+async def delete_budget():
