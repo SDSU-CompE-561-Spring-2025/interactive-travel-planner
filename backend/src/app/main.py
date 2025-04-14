1896136a7a397e54ffc66956231aef3a0fe17be3
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 19:35:41 -0700

    edited main.py file

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import uvicorn
-from src import app  # src/__init__.py must define: app = FastAPI()
-
-if __name__ == "__main__":
-    uvicorn.run("src:app", host="127.0.0.1", port=8000, reload=True)
-=======
 from fastapi import FastAPI
 from app.core.database import Base, engine
 from app.routes import api_router
@@ -21,4 +14,3 @@ app.include_router(api_router, prefix="")
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
->>>>>>> 5ad872a33590a78f48a24bc9a4520c6de93b2fb2
