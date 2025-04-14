07ac2399f11bb4c02b9f2253f0420989150332dc
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 19:28:27 -0700

    added api router

--- backend/src/app/routes/__init__.py
+++ backend/src/app/routes/__init__.py
@@ -1,6 +1,7 @@
 from app.routes import category, user, trips, destinations, itinerary, dates, calendar
 
 api_router = APIRouter()
+
 api_router.include_router(user.router, prefix="/auth", tags=["User"])
 api_router.include_router(category.router, prefix="/category", tags=["Category"])
 api_router.include_router(trips.router, tags=["Trips"])
@@ -8,4 +9,6 @@ api_router.include_router(destinations.router, tags=["Destinations"])
 api_router.include_router(calendar.router, tags=["Calendar"])
 api_router.include_router(itinerary.router, tags=["Itinerary"])
 api_router.include_router(dates.router, tags=["Dates"])
+
+app.include_router(api_router)
 
