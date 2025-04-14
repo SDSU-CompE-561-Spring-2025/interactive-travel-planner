9093b9af1b8e3588aa53146be2e11d5b663d36c0
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 19:05:47 -0700

    fixed small naming error

--- backend/src/app/services/calendar.py
+++ backend/src/app/services/calendar.py
@@ -13,7 +13,7 @@ def get_events(db: Session, user_id: int):
     return db.query(CalendarEvent).filter(CalendarEvent.user_id == user_id).all()
 
 def get_event(db: Session, event_id: int, user_id:int):
-    return db.query(CalendarEvent).filter(CalendarEvent.user_id == event_id, CalendarEvent.user_id == user_id).first()
+    return db.query(CalendarEvent).filter(CalendarEvent.id == event_id, CalendarEvent.user_id == user_id).first()
 
 def delete_event(db: Session, event_id: int, user_id: int):
     event = get_event(db, event_id, user_id)
