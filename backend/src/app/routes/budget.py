b57a915bd159c04cbd5206684fc19620abc0c045
kohkikita <130106015+kohkikita@users.noreply.github.com>
2025-04-13 21:05:13 -0700

    updated budget routes

---
+++ backend/src/app/routes/budget.py
@@ -0,0 +1,8 @@
+fromfrom fastapi import APIRouter
+from app.schemas.budget import 
+from app.dependencies import get_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+route = APIRouter()
+
