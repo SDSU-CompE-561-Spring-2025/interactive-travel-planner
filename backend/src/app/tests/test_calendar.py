e8d2faa71861d4c6bcad44bb53dd5ef93bf191a6
Ethan Quach <ethanquachwork@gmail.com>
2025-04-13 21:58:43 -0700

    send login request to get token for test

--- backend/src/app/tests/test_calendar.py
+++ backend/src/app/tests/test_calendar.py
@@ -1,3 +1,13 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app # adjust path to FastAPI
+
+client = TestClient(app)
+
+def get_auth_token():
+
+    # Send login request to get token
+    response = client.post(
+        "/auth/login",
+         data={"username": "testuser", "password": "testpassword"},
+    )
