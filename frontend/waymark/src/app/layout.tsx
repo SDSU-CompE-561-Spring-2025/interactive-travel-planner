73a2bba6878693f7a9f0b786e890338871a69ae4
RojinOsman <rojinosman1988@gmail.com>
2025-04-20 22:16:02 -0700

    Started the Next.js front end. finished up to demo from April 9th class

---
+++ frontend/waymark/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Waymark",
+  description: "A travel itinerary planner to help plan your next trip",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-center text-5xl p-5`}
+      >
+        <h1> NavBar</h1>
+        {children}
+        <footer> Footer </footer>
+      </body>
+    </html>
+  );
+}
