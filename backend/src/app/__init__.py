42bf8af00722605fdf28f72a5a0c72e5d132dcba
Ethan Quach <ethanquachwork@gmail.com>
2025-04-22 14:19:59 -0700

    edited main init file:

--- backend/src/app/__init__.py
+++
@@ -1,5 +0,0 @@
-fromt fastapi import APIRouter
-from app.routers import collaboration
-
-api_router = APIRouter()
-api_router.include_router(collaboration.router, prefix="/api", tags=["collaboration"])
