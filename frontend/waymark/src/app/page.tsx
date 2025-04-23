1fea04f279661d6f80dcf841d777cbc23f45c751
RojinOsman <rojinosman1988@gmail.com>
2025-04-23 12:23:52 -0700

    added husky dependency

--- frontend/waymark/src/app/page.tsx
+++ frontend/waymark/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1> Home Page </h1>
+      <h1 className="text-9xl text-amber-400 bg-orange-50 pt-5 pb-5 m-10"> Home Page </h1>
     </main>
   );
 }
--- frontend/waymark/src/app/support/layout.tsx
+++ frontend/waymark/src/app/support/layout.tsx
@@ -4,8 +4,8 @@ export default function SupportLayout({
     children: React.ReactNode;
     }>) {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-4">
-        <h1 className="text-3xl font-bold">Support</h1>
+        <div className="flex flex-col items-center justify-center">
+        <h1>Support Layout</h1>
         {children}
         </div>
     );
