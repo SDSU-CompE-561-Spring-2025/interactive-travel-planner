bcdaa1cc0df73f852025a1cf8a86d05b1eb270b3
Ethan Quach <ethanquachwork@gmail.com>
2025-05-05 14:02:48 -0700

    Created trips page

--- frontend/waymark/src/trips/page.tsx
+++ frontend/waymark/src/trips/page.tsx
@@ -1,8 +1,9 @@
 export default function TripsPage() {
     return (
-        <main className = "p-6">
-            <h1 className = "text-3xl font-bold mb-4">My Trips</h1>
-            <p className = "text-gray-700">This is where trips will be displayed.</p>
-        </main>    
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-4">My Trips</h1>
+        <p className="text-gray-700">This is where trips will be displayed.</p>
+      </main>
     );
-}
+  }
+  
