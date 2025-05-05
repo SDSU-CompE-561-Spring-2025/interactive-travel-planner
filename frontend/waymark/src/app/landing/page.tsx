56bee86547dff4a8c007723bba8a9f77b1f3a8ca
RojinOsman <rojinosman1988@gmail.com>
2025-05-04 23:20:10 -0700

    added the create new trip page

---
+++ frontend/waymark/src/app/landing/page.tsx
@@ -0,0 +1,17 @@
+import Title from "@/components/Title";
+
+
+
+export default function Home() {
+    return (
+        <main>
+        <Title
+            title="landing"
+            subtitle="landing page"
+        />
+        <div className ="flex justify-center">
+        </div>
+        </main>
+
+    );
+}
