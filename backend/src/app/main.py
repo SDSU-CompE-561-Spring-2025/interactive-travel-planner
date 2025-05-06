04b4532249a882af1739e14fde2310d4d23992e6
RojinOsman <rojinosman1988@gmail.com>
2025-05-06 16:52:11 -0700

    added to backend for frontend connection

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -2,11 +2,21 @@ from fastapi import FastAPI
 
 from app.core.database import Base, engine
 from app.routes import api_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(api_router, prefix="")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # your frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
