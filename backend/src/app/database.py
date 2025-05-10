3c0859c585b9f702fd7a31cbe9cbb115e505269c
RojinOsman <rojinosman1988@gmail.com>
2025-05-10 14:06:53 -0700

    docker connection fixed

--- backend/src/app/database.py
+++ backend/src/app/database.py
@@ -1,10 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE_URL = 'sqlite:///waymark_app.db'
 
-engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./waymark_app.db"
+)
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
